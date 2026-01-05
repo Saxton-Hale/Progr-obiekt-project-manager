102913f867bbf0ad0f6fef81b85a0aa14783a47d
unknown <robert@madmax.consulting>
2026-01-05 17:11:14 +0100

    feat: in-memory repositories and unit of work in infrastructure

---
+++ TaskBoard.Infrastructure/Repositories/BoardRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskBoard.Domain.Entities;
+using TaskBoard.Domain.Interfaces;
+
+namespace TaskBoard.Infrastructure.Repositories
+{
+    internal sealed class BoardRepository : IBoardRepository
+    {
+        private readonly List<Board> _boards = new();
+
+        public void Add(Board board)
+        {
+            if (board is null) throw new ArgumentNullException(nameof(board));
+
+            if (_boards.Any(b => b.Id == board.Id))
+                throw new InvalidOperationException("Board with the same Id already exists");
+
+            _boards.Add(board);
+        }
+
+        public IEnumerable<Board> GetAll()
+        {
+            return _boards.ToList();
+        }
+
+        public Board? GetById(Guid id)
+        {
+            return _boards.SingleOrDefault(b => b.Id == id);
+        }
+
+        public void Remove(Board board)
+        {
+            if (board is null) throw new ArgumentNullException(nameof(board));
+
+            var existing = _boards.SingleOrDefault(b => b.Id == board.Id);
+            if (existing is null)
+                throw new InvalidOperationException("Board not found");
+
+            _boards.Remove(existing);
+        }
+
+        public void Update(Board board)
+        {
+            if (board is null) throw new ArgumentNullException(nameof(board));
+
+            var index = _boards.FindIndex(b => b.Id == board.Id);
+            if (index < 0)
+                throw new InvalidOperationException("Board not found");
+
+            _boards[index] = board;
+        }
+    }
+}
---
+++ TaskBoard.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskBoard.Domain.Interfaces;
+
+namespace TaskBoard.Infrastructure.Repositories
+{
+    internal sealed class UnitOfWork : IUnitOfWork
+    {
+        public IBoardRepository Boards { get; }
+
+        public UnitOfWork()
+        {
+            Boards = new BoardRepository();
+        }
+
+        public void SaveChanges()
+        {
+            //in memory wiec zostaje tutaj puste
+        }
+    }
+}
